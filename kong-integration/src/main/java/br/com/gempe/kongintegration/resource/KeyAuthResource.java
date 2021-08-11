c067f5f0931faec23f7070ff53992fa1ae9faa15
Tiago Liberatti <tiagoliberatti@gempe.dev>
2021-08-11 17:41:00 -0300

    refactor: removendo uma barra no path da api

--- kong-integration/src/main/java/br/com/gempe/kongintegration/resource/KeyAuthResource.java
+++ kong-integration/src/main/java/br/com/gempe/kongintegration/resource/KeyAuthResource.java
@@ -13,7 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(value = "v1/kong/servicos/")
+@RequestMapping(value = "v1/kong/servicos")
 public class KeyAuthResource {
 
     private CreateConsumerService createConsumerService;
