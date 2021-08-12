2e4661f8b151d4f50e91ca2c31c4360d574e5ae5
Tiago Liberatti <tiagoliberatti@gempe.dev>
2021-08-12 13:55:05 -0300

    feat: docker-compose para o serviço da api, correções para comunicação entre os containers

--- kong-integration/src/main/java/br/com/gempe/kongintegration/KongIntegrationApplication.java
+++ kong-integration/src/main/java/br/com/gempe/kongintegration/KongIntegrationApplication.java
@@ -1,11 +1,17 @@
 package br.com.gempe.kongintegration;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableAutoConfiguration
+@Configuration
+@ComponentScan
 public class KongIntegrationApplication {
 
 	public static void main(String[] args) {
